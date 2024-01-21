ea8b9c28955d5180801e481e6dd33e1ae8618ee0
dhrvjha <kdhruv71@gmail.com>
2024-01-21 23:50:26 +0530

    updated settings to get secrets from env file

--- dhrvjha/settings.py
+++ dhrvjha/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a8guwy(2s#rf$6%n0k7quh_(u6i_*r4*t2urk5vay$fhkv)k5_'
-
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0)) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
